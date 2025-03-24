617737a51be0ea45d7076ebe2ecec11df2b4fe1c
Deniz Aydemir <deniz@aydemir.us>
2025-03-23 20:13:13 -0400

    Clean up: Remove Terraform, App Runner, and Docker configurations, moving to Render deployment

--- terraform/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "app_runner_service_url" {
-  description = "The URL of the App Runner service"
-  value       = module.apprunner.service_url
-}
-
-output "app_runner_service_status" {
-  description = "The status of the App Runner service"
-  value       = module.apprunner.service_status
-}
